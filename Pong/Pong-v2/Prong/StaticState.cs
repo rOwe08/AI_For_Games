753b45a27fd55c6fe486df10742237395f951b26
Igor MInenko <igorymin2002@gmail.com>
2024-01-12 14:39:47 +0100

    Adding super mario solution for the first task

---
+++ Pong/Pong-v2/Prong/StaticState.cs
@@ -0,0 +1,24 @@
+﻿namespace Prong
+{
+    public class StaticState
+    {
+        public int ClientSize_Width = 0;
+        public int ClientSize_Height = 0;
+        public int gridCellSize = 10;
+        public int paddleWidthCells = 2;
+        public int paddleHeightCells = 10;
+        public float ballSpeed = 900;
+        public float paddle1Speed = 500;
+        public float paddle2Speed = 500;
+
+        public int paddleWidth()
+        {
+            return gridCellSize * paddleWidthCells;
+        }
+
+        public int paddleHeight()
+        {
+            return gridCellSize * paddleHeightCells;
+        }
+    }
+}
